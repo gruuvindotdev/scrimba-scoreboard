a1d533c8bea7a62589144e12b9d6ee8558434f1d
Gruuvin <dirk@elimer.de>
2025-02-09 19:48:13 +0100

    add updateLeader function, which add a green border around the leading teams scoreboard.

--- index.js
+++ index.js
@@ -7,31 +7,51 @@ let ctnGuest = document.getElementById("ctn-guest")
 ctnHome.textContent = scoreHome
 ctnGuest.textContent = scoreGuest
 
+function updateLeader() {
+    if (scoreHome > scoreGuest) {
+        ctnHome.style.border = "3px solid #43DD76"
+        ctnGuest.style.border = "none"
+    } else if (scoreGuest > scoreHome) {
+        ctnHome.style.border = "none"
+        ctnGuest.style.border = "3px solid #43DD76"
+    } else {
+        ctnHome.style.border = "none"
+        ctnGuest.style.border = "none"
+    }
+}
+
+
 function addHome1() {
     scoreHome += 1
     ctnHome.textContent = scoreHome
+    updateLeader()
 }
 function addHome2() {
     scoreHome += 2
     ctnHome.textContent = scoreHome
+    updateLeader()
 }
 function addHome3() {
     scoreHome += 3
     ctnHome.textContent = scoreHome
+    updateLeader()
 }
 
 function addGuest1() {
     scoreGuest += 1
     ctnGuest.textContent = scoreGuest
+    updateLeader()
 }
 function addGuest2() {
     scoreGuest += 2
     ctnGuest.textContent = scoreGuest
+    updateLeader()
 }
 
 function addGuest3() {
     scoreGuest += 3
     ctnGuest.textContent = scoreGuest
+    updateLeader()
 }
 
 function newGame() {
@@ -39,4 +59,9 @@ function newGame() {
     scoreGuest = 0
     ctnHome.textContent = scoreHome
     ctnGuest.textContent = scoreGuest
+    updateLeader()
+}
+
+if (scoreHome > scoreGuest) {
+    ctnHome.style.border = "3px solid green"
 }
