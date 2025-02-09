2dceabff073b6dc9759d450dea36522fe58734f3
Gruuvin <dirk@elimer.de>
2025-02-09 18:28:46 +0100

    add todos

--- index.js
+++ index.js
@@ -4,24 +4,34 @@ let scoreGuest = 0
 let ctnHome = document.getElementById("ctn-home")
 let ctnGuest = document.getElementById("ctn-guest")
 
+ctnHome.textContent = scoreHome
+ctnGuest.textContent = scoreGuest
 
 function addHome1() {
-    console.log("clicked+1")
+    scoreHome += 1
+    ctnHome.textContent = scoreHome
 }
 function addHome2() {
-    console.log("clicked+2")
+    scoreHome += 2
+    ctnHome.textContent = scoreHome
 }
 function addHome3() {
-    console.log("clicked+3")
+    scoreHome += 3
+    ctnHome.textContent = scoreHome
 }
 
 function addGuest1() {
-    console.log("clicked+1")
+    scoreGuest += 1
+    ctnGuest.textContent = scoreGuest
 }
 function addGuest2() {
-    console.log("clicked+2")
+    scoreGuest += 2
+    ctnGuest.textContent = scoreGuest
 }
+
+// FIXME: add another function
 function addGuest3() {
-    console.log("clicked+3")
+    scoreGuest += 3
+    ctnGuest.textContent = scoreGuest
 }
 
