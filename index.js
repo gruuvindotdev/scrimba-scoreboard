48def3ba9ad070d0fc2128280e962ccd04f3978c
Gruuvin <dirk@elimer.de>
2025-02-09 18:06:27 +0100

    add javascript intial structure, set variables, declare the button functions.

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 let scoreHome = 0
 let scoreGuest = 0
 
+let ctnHome = document.getElementById("ctn-home")
+let ctnGuest = document.getElementById("ctn-guest")
+
+
+function addHome1() {
+    console.log("clicked+1")
+}
+function addHome2() {
+    console.log("clicked+2")
+}
+function addHome3() {
+    console.log("clicked+3")
+}
+
+function addGuest1() {
+    console.log("clicked+1")
+}
+function addGuest2() {
+    console.log("clicked+2")
+}
+function addGuest3() {
+    console.log("clicked+3")
+}
+
